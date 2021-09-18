9a9f5ea487cf4fe87bbb0f5d3564d8a92ac756e8
kdnakt <a.kid.1985@gmail.com>
2021-09-18 18:43:28 +0900

    assert path is a file

--- file-path-matcher/spec/filepath_spec.sh
+++ file-path-matcher/spec/filepath_spec.sh
@@ -1,9 +1,22 @@
 Describe 'File path matcher'
   createDir() { mkdir target; }
+  createFile() { mkdir target; touch target/${1}; }
   cleanup() { rm -r ./target; }
-  AfterAll cleanup
-  It 'creates directory'
-    When call createDir
-    The path ./target should be directory
+
+  Describe 'directory'
+    AfterAll cleanup
+    It 'creates directory'
+      When call createDir
+      The path ./target should be directory
+      The path ./target should be empty directory
+    End
+  End
+
+  Describe 'file'
+    AfterAll cleanup
+    It 'creates a file'
+      When call createFile hoge
+      The path ./target/hoge should be empty file
+    End
   End
 End
