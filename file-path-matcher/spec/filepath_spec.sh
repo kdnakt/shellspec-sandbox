475d421163bebcc8b1b386f9dda88f71323d3cb2
kdnakt <a.kid.1985@gmail.com>
2021-09-19 08:28:14 +0900

    check file permission

--- file-path-matcher/spec/filepath_spec.sh
+++ file-path-matcher/spec/filepath_spec.sh
@@ -7,6 +7,7 @@ Describe 'File path matcher'
     AfterAll cleanup
     It 'creates directory'
       When call createDir
+      The path ./target should be exist
       The path ./target should be directory
       The path ./target should be empty directory
     End
@@ -16,7 +17,12 @@ Describe 'File path matcher'
     AfterAll cleanup
     It 'creates a file'
       When call createFile hoge
+      The path ./target/hoge should be exist
+      The path ./target/hoge should be file
       The path ./target/hoge should be empty file
+      The path ./target/hoge should be readable
+      The path ./target/hoge should be writable
+      The path ./target/hoge should not be executable
     End
   End
 End
