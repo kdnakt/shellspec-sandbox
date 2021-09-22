2388048f891293855476c4ae0f241cf368814310
kdnakt <a.kid.1985@gmail.com>
2021-09-22 23:13:12 +0900

    try various modifiers

--- error-subject/spec/error_spec.sh
+++ error-subject/spec/error_spec.sh
@@ -1,9 +1,18 @@
 Describe 'error subject'
-  hello() { echo 'hoge' >&2; }
+  hello() {
+    echo 'hoge fuga' >&2;
+    echo 'foo bar' >&2;
+  }
 
   It 'says hoge in error'
     When call hello
-    The error should equal 'hoge'
-    The stderr should equal 'hoge'
+    The error should equal 'hoge fuga
+foo bar'
+    The stderr should equal 'hoge fuga
+foo bar'
+    The lines of error should equal 2
+    The line 2 of error should equal 'foo bar'
+    The word 2 of error should equal 'fuga'
+    The length of error should equal 17
   End
 End
